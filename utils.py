10632de31356b8e91e05c7688cfb14f487b47c88
Akshat009-hash <akshatofficial63@gmail.com>
2025-08-03 22:31:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -88,12 +88,13 @@ async def progress_bar(current, total, reply, start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "█" * completed_length + "░" * remaining_length
-            
+            progress_bar = "▰" * completed_length + "▱" * remaining_length
+           
             try:
                 await reply.edit(f'<b>\n ╭──⌯════🆄︎ᴘʟᴏᴀᴅɪɴɢ⬆️⬆️═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├🤖 𝔹ʏ » ＳＰＩＤΣＲ⚡\n╰─═══ ✪ @ＳＰＩＤΣＲ⚡ ✪ ═══─╯\n</b>') 
             except FloodWait as e:
                 time.sleep(e.x)
+
 
 
 
