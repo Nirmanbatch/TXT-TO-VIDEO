8e5b9fdf322862f950715b4bae8c265c3bf2a023
Akshat009-hash <akshatofficial63@gmail.com>
2025-08-03 16:17:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def stop_bot():
     await bot.stop()
 
 async def main():
-    if WEBHOOK:
+    if WEBHOOK:false
         # Start the web server
         app_runner = web.AppRunner(await web_server())
         await app_runner.setup()
